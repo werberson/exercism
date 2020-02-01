2692278dc610bcbe7287cf39715c743d4be2bbad
Werberson Silva <werberson.silva@gmail.com>
2020-01-31 23:43:11 -0300

    refactoring and fix typos

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -3,17 +3,17 @@ package hamming
 
 import "errors"
 
-// Distance given two DNA sequence, return the Hamming Distance between them.
+// Distance given two DNA sequences, return the Hamming Distance between them.
 // returns error if the sequences does not have equal lengths
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
-		return 0, errors.New("the sequences must have equal lengths")
+		return 0, errors.New("the sequences a and b must have equal lengths")
 	}
-	distance := 0
+	count := 0
 	for i := 0; i < len(a); i++ {
 		if a[i] != b[i] {
-			distance++
+			count++
 		}
 	}
-	return distance, nil
+	return count, nil
 }
