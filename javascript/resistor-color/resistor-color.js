3117f19839b949401a7e74b5139a908e19c97bdb
Werberson Silva <werberson.silva@gmail.com>
2020-02-03 23:18:30 -0300

    resistor color solution JS

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,9 @@
+/**
+ * Returns the code for a color.
+ * @param color The color to get the code.
+ */
+export const colorCode = (color) => {
+    return COLORS.indexOf(color)
+};
+
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
