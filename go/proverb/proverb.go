6dcb627d49441372d5bb2c248e91ab1b6953876d
Werberson Silva <werberson.silva@gmail.com>
2020-02-05 00:07:15 -0300

    proverb solutions - GO

---
+++ go/proverb/proverb.go
@@ -0,0 +1,20 @@
+// Package proverb implements the proverb generator function
+package proverb
+
+import "fmt"
+
+// Proverb given an array of rhyme , generate the relevant proverb 'For Want of a Nail'
+func Proverb(rhyme []string) []string {
+	proverb := make([]string, len(rhyme))
+	lastIndex := len(rhyme) - 1
+	for i, r := range rhyme {
+		var message string
+		if i == lastIndex {
+			message = fmt.Sprintf("And all for the want of a %s.", rhyme[0])
+		} else {
+			message = fmt.Sprintf("For want of a %s the %s was lost.", r, rhyme[i+1])
+		}
+		proverb[i] = message
+	}
+	return proverb
+}
