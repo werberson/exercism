229d19e7140e197db772630a126f8fd0d226f2b6
Werberson Silva <werberson.silva@gmail.com>
2020-02-06 23:23:01 -0300

    resistor color duo solution JS

---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,21 @@
+const colorCode = {
+    black: "0",
+    brown: "1",
+    red: "2",
+    orange: "3",
+    yellow: "4",
+    green: "5",
+    blue: "6",
+    violet: "7",
+    grey: "8",
+    white: "9"
+};
+
+/**
+ * Decode the resistor color names as two digit number.
+ * @param colors represents the colors of the first 2 or more color bands of a resistor
+ * @returns {number} the combined number value of the first 2 color bands
+ */
+export const decodedValue = (colors) => {
+    return  parseInt(colorCode[colors[0]] + colorCode[colors[1]])
+};
