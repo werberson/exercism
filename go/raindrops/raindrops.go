3e7cda363895a72b6fb8068806de3c51834d9cb8
Werberson Silva <werberson.silva@gmail.com>
2020-02-09 12:52:53 -0300

    golint

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,16 +1,18 @@
+// Package raindrops provides function to convert a number into raindrop string sounds
 package raindrops
 
 import "strconv"
 
+// Convert convert a number into a string that contains raindrop sounds corresponding to certain potential factors.
 func Convert(num int) string {
 	var str string
-	if num% 3 == 0 {
+	if num%3 == 0 {
 		str = "Pling"
 	}
-	if num% 5 == 0 {
+	if num%5 == 0 {
 		str += "Plang"
 	}
-	if num% 7 == 0 {
+	if num%7 == 0 {
 		str += "Plong"
 	}
 	if str == "" {
