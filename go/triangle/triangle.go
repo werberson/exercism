a6cd3810b4236f7980f76fb18ed3590207259683
Werberson Silva <werberson.silva@gmail.com>
2020-02-09 13:47:23 -0300

    math.IsInf(x, 0) == (math.IsInf(x, 1) && math.IsInf(x, -1))

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -20,15 +20,7 @@ const (
 // Sca scalene if has all sides of different lengths.
 // NaT Not a triangle if has invalid triangle sides
 func KindFromSides(a, b, c float64) Kind {
-	if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) {
-		return NaT
-	}
-	if math.IsInf(a, 1) || math.IsInf(a, -1) ||
-		math.IsInf(b, 1) || math.IsInf(b, -1) ||
-		math.IsInf(c, 1) || math.IsInf(c, -1) {
-		return NaT
-	}
-	if a <= 0 || b <= 0 || c <= 0 || (a+b < c || a+c < b || b+c < a) {
+	if isNotTriangle(a, b, c) {
 		return NaT
 	}
 
@@ -41,4 +33,16 @@ func KindFromSides(a, b, c float64) Kind {
 		k = Sca
 	}
 	return k
+}
+
+func isNotTriangle(a, b, c float64) bool {
+	for _, v := range []float64{a, b, c} {
+		if math.IsNaN(v) || math.IsInf(v, 0) {
+			return true
+		}
+	}
+	if a <= 0 || b <= 0 || c <= 0 || (a+b < c || a+c < b || b+c < a) {
+		return true
+	}
+	return false
 }
